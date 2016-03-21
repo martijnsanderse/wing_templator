b2c86fd220fc5a4259f6d3a1031c8c8e8bd9820c
Martijn Sanderse <martijnsanderse@gmail.com>
2016-03-21 22:02:36 +0100

    Now using numpy, scipy and matplotlib.

--- template.py
+++ template.py
@@ -1,32 +1,47 @@
 import sys # for argv
-from PIL import Image, ImageDraw
+from scipy import interpolate
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def main():  
-    filename = sys.argv[1]
-    with open(filename, "rb") as f:
-        lines = f.readlines()
+filename = sys.argv[1]
+with open(filename, "rb") as f:
+    lines = f.readlines()
 
-    print "Airfoil: ", lines[0]
-    coordinates = [[float(coordinate) for coordinate in line.split()] for line in lines[1:]]
+print "Airfoil: ", lines[0]
+coordinates = [[float(coordinate) for coordinate in line.split()] for line in lines[1:]]
 
-    # x, y = zip(*coordinates)
-    # minX = min(x)
-    # maxX = max(x)
-    # minY = min(y)
-    # maxY = max(y)
+l = len(coordinates)
+x, y = zip(*coordinates)
 
-    im = Image.new("RGB", (400, 400), "white")
-    draw = ImageDraw.Draw(im)
-    
-    for i in range(1, len(coordinates)):
-        draw.line((coordinates[i-1][0]*200+50,
-        			coordinates[i-1][1]*200+200,
-        			coordinates[i][0]*200+50,
-        			coordinates[i][1]*200+200), fill="black")
-    
-    im.save("test.png")
+x_top = np.array(x[0:l/2+1])
+y_top = np.array(y[0:l/2+1])
 
+x_bot = np.array(x[l/2:])
+y_bot = np.array(y[l/2:])
 
-if __name__ == "__main__":
-    main()
+template_height = 0.1 # should become an absolute value after scaling
+template_lead_width = 0.1 # idem
+x_tem = np.array([x_top[0],
+                    x_top[0]+template_lead_width, 
+                   x_top[0]+template_lead_width,
+                   x_top[-1]-template_lead_width,
+                   x_top[-1]-template_lead_width,
+                   x_top[-1]])
+y_tem = np.array([y_top[0],
+                    y_top[0],
+                  y_top[0]-template_height,
+                  y_top[-1]-template_height,
+                  y_top[-1],
+                  y_top[-1]])
+
+print x_top[0], y_top[0]
+print x_top[-1], y_top[-1]
+print x_bot[0], y_bot[0]
+print x_bot[-1], y_bot[-1]
+
+plt.plot(x_bot, y_bot)
+plt.plot(x_top, y_top)
+plt.plot(x_tem, y_tem)
+plt.axis('equal')
+plt.show()
