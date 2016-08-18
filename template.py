ea8920a5930aed83d4f57554e9659e9add6611a5
Martijn Sanderse <martijnsanderse@gmail.com>
2016-08-18 21:00:25 +0200

    Improved translate, although rotation does not seem to be working yet. also improved split up, but not yet there...

--- template.py
+++ template.py
@@ -8,24 +8,36 @@ def read_coordinates(filename):
         lines = f.readlines()
 
     print ("Airfoil: ", lines[0])
-    coordinates = [[float(coordinate) for coordinate in line.split()] for line in lines[1:]]
+    coordinates = numpy.array(
+        [[float(coordinate) for coordinate in line.split()] for line in lines[1:]]
+        )
 
-    x, y = zip(*coordinates)
+    return coordinates
 
-    return x, y
-
-def transform(x, y):
+def transform(coordinates, scale, translation):
     # todo: use numpy matrix multiplication
     # for affine transformations.
-    x_temp = []
-    y_temp = []
-    for x_t in x:
-        transformed = x_t * 100 + 30
-        x_temp.append(transformed)
-    for y_t in y:
-        transformed = y_t * -100 + 10
-        y_temp.append(transformed)
-    return x_temp, y_temp
+
+    # scaling
+    s_x = scale[0] # mm
+    s_y = -scale[1] # mm
+
+    # translation
+    t_x = translation[0]
+    t_y = translation[1]
+
+    #rotation does not seem to work properly?
+    a = 0.0 # degrees
+    r = a * numpy.pi/180
+
+    # clockwise rotation
+    scale_rotate = numpy.matrix([[s_x * numpy.cos(r),   -numpy.sin(r)    ],
+                                 [numpy.sin(r),        s_y*numpy.cos(r)]])
+    translate = numpy.matrix([t_x, t_y])
+
+    c = numpy.matrix(coordinates) * scale_rotate + translate
+
+    return numpy.array(c)
 
 def box_lines(dwg, x, y):
     """Returns the lines that form all of the straight edges."""
@@ -60,17 +72,18 @@ def box_lines(dwg, x, y):
 
     return lines
 
-def upper_half_lines(dwg, x, y):
+def upper_half_lines(dwg, coordinates):
     """returns lines that form the upper half of the airfoil."""
-    half_l = int(len(x)/2)
+
+    x, y = zip(*coordinates)
 
     lines = []
-    for i in range(half_l-1):
+    for i in range(len(x)-1):
         x1 = "{}".format(x[i])
         y1 = "{}".format(y[i])
         x2 = "{}".format(x[i+1])
         y2 = "{}".format(y[i+1])
-     
+
         lines.append(
             dwg.line( 
                 (x1, y1), 
@@ -81,21 +94,16 @@ def upper_half_lines(dwg, x, y):
             )
     return lines
 
-def draw_template(filename):
+def draw_upper_template(dwg, filename, size, offset):
+    coordinates = read_coordinates(filename)
+    coordinates = transform(coordinates, size, offset)
 
-    x, y = read_coordinates(filename)
-    x, y = transform(x, y)
+    x, y = zip(*coordinates)
 
     l = len(x)
     half_l = int(l/2)
 
-    dwg = svgwrite.Drawing(
-        'test.svg', 
-        profile='tiny', 
-        size=('170mm', '130mm'), 
-        viewBox=('0 0 170 130'))    
-
-    for line in upper_half_lines(dwg, x, y):
+    for line in upper_half_lines(dwg, coordinates[0:half_l+1]):
         dwg.add(line)
 
     for line in box_lines(dwg, x, y):
@@ -113,8 +121,49 @@ def draw_template(filename):
             fill_opacity="0.0")
         )
 
+def draw_lower_template(dwg, filename, size, offset):
+    coordinates = read_coordinates(filename)
+    coordinates = transform(coordinates, size, offset)
+
+    print (coordinates.shape)
+    l = coordinates.shape[0]
+    half_l = int(l/2)
+
+    for line in upper_half_lines(dwg, coordinates[half_l:]):
+        dwg.add(line)
+
+    # for line in box_lines(dwg, x, y):
+    #     dwg.add(line)
+
+    # dwg.add(
+    #     dwg.path(
+    #         d = "M {},{} C {} {}, {} {}, {} {}".format(
+    #             x[half_l], y[half_l],
+    #             x[half_l]-5, y[half_l]+5,
+    #             x[half_l]-5, y[half_l]+5,
+    #             10,y[0]+5),
+    #         stroke = "black",
+    #         stroke_width = "0.1",
+    #         fill_opacity="0.0")
+    #     )
+
+def draw_template(filename, size):
+
+    dwg = svgwrite.Drawing(
+        'test.svg', 
+        profile='tiny', 
+        size=('170mm', '130mm'),
+        viewBox=('0 0 170 130'))
+
+    # the offset is just chosen so that the templates 
+    # fit on the paper, and do not overlap
+    draw_upper_template(dwg, sys.argv[1], (100,100), (25,10))
+    draw_lower_template(dwg, sys.argv[1], (100,100), (25,40))
+
     dwg.save()
 
 if __name__ == "__main__":
 
-    draw_template(sys.argv[1])
+    draw_template(sys.argv[1], (100,100))
+
+    
